66c088f75071e06e5aa000c2bcb86924fcd175e0
Betelhem-Belete <betelhembelete0@gmail.com>
2023-12-17 19:25:23 -0500

    tati login/signup img done

--- admins/register.php
+++ admins/register.php
@@ -61,8 +61,9 @@
         <div class="card-body p-0">
           <!-- Nested Row within Card Body -->
           <div class="row">
-            <div class="col-lg-5 d-none d-lg-block "></div>
-            <div class="col-lg-7">
+            <div class="col-lg-6 d-none d-lg-block">
+                                <img src="../images/realestatelogin.jpg" alt="" style='width:550px; height: 490px;'>
+            </div>
               <div class="p-5">
                 <div class="text-center">
                   <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
