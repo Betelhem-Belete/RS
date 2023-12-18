2c542f92b000093e0d1df093eccde75d26ee519e
Betelhem-Belete <betelhembelete0@gmail.com>
2023-12-17 20:52:39 -0500

    contactUs page with location and db connection done

---
+++ config/config.php
@@ -0,0 +1,27 @@
+<?php
+try{
+    //host 
+    define('HOSTNAME', 'localhost');
+
+    //DBNAME
+    define('DBNAME','haylu');
+
+    //user
+    define('USER', 'root');
+
+    //pass
+    define('PASS', '');
+
+    $conn = new PDO("mysql:host=".HOSTNAME.";dbname=".DBNAME.";",USER, PASS);
+    $conn->ssetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // if($conn == true) {
+    //     echo 'db connected';
+    // } else {
+    //     echo 'error';
+    // }
+    } catch(PDOException $e){
+        die("database connection failed: ", $e->getMessage());
+    }
+
+    ?>
--- index.php
+++ index.php
@@ -612,14 +612,23 @@
     </div>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-6 px-0">
+      <div class="col-md-6 px-0">
           <div class="map_container">
             <div class="map-responsive">
+              <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAoSJEs6ztIcy4DhKa3AXvpreEdH3pC3Eg&q=Ethiopian+AddisAbaba+BOLE" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
               <!-- <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe> -->
-            </div>
+                <!-- <iframe
+                  width="600"
+                  height="600"
+                  frameborder="0" style="border:0"
+                  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAoSJEs6ztIcy4DhKa3AXvpreEdH3pC3Eg&q=Ethiopian+AddisAbaba" allowfullscreen>
+                </iframe> -->
+              </div>
+            
+
           </div>
         </div>
-        <div class="col-lg-4 col-md-5 ">
+        <div class="col-lg-6 col-md-5 ">
           <div class="form_container">
             <form action="">
               <div>
@@ -635,7 +644,7 @@
                 <input type="text" class="message-box" placeholder="Message" />
               </div>
               <div class="d-flex ">
-                <button>
+                <button class='btn btn-block'>
                   Send
                 </button>
               </div>
