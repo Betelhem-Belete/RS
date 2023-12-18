6b8bfb25b6c2eb52bab71c7736b85ffa30baad50
tati <tatitaye0@gmail.com>
2023-12-18 20:21:50 +0300

    user edit ui complited

--- admins/addEmp.php
+++ admins/addEmp.php
@@ -277,35 +277,6 @@
                 <?php
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $firstname = $_POST['firstname'];
-
-                    // try {
-                    //     // host
-                    //     define('HOSTNAME', 'localhost');
-
-                    //     // DBNAME
-                    //     define('DBNAME', 'haylu');
-
-                    //     // user
-                    //     define('USER', 'root');
-
-                    //     // pass
-                    //     define('PASS', '');
-
-                    //     $conn = new PDO("mysql:host=" . HOSTNAME . ";dbname=" . DBNAME . ";", USER, PASS);
-                    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                    //     // Check if the connection is successful
-                    //     if ($conn) {
-                    //         echo 'Database connected successfully  ';
-                    //     } else {
-                    //         echo 'Database connection failed';
-                    //     }
-                    // } catch (PDOException $e) {
-                    //     die("Database connection failed: " . $e->getMessage());
-                    // }
-
-
-
                     // Function to validate input using regular expressions
                     function validateInput($input, $pattern)
                     {
